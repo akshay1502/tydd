37deb283cecec0c9dab398a926699941b8fdcd54
akshay <akshay.shinde@ly.design>
2025-02-15 17:17:22 +0530

    added linear gradient

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config = {
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
@@ -10,19 +11,71 @@ const config = {
   theme: {
     extend: {
       colors: {
-        blue: '#2986FD', // Example custom color
-        black: '#1A1A1A', // Another custom color
+        blue: '#2986FD',
+        black: '#1A1A1A',
         subTitle: '#8D8D8D',
-        white: '#FFFFFF', // Example accent color
+        white: '#FFFFFF',
         offWhite: '#BEBEBE',
         darkBlue: '#091E42',
         borderStroke: '#6C6C6C',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
       },
       boxShadow: {
         packages: '4px 4px 13px 4px rgba(0, 0, 0, 0.15)',
       },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      backgroundImage: {
+        'last-minute-card-gradient':
+          'linear-gradient(113deg, rgba(255, 255, 255, 0.32) -3.49%, rgba(255, 255, 255, 0.10) 114.11%)',
+        'last-minute-discount-gradient':
+          'linear-gradient(114deg, #E42125 -43.53%, #6F8CC8 105.61%)',
+      },
     },
   },
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config
 
 export default config
