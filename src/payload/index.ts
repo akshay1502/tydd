09b47040122c56651795e2bd765b395d781985cf
akshay <akshay.shinde@ly.design>
2025-02-25 22:36:43 +0530

    added types declarations

--- src/payload/index.ts
+++ src/payload/index.ts
@@ -1,16 +1,17 @@
 import { CollectionSlug, getPayload } from 'payload'
 import config from '@/payload.config'
+import { PopularPackages } from '@/types/global'
 
 // initializing payload to be used for local queries
 const payload = await getPayload({ config })
 
-export const getDetailPage = async (slug: CollectionSlug, id: string) => {
+export const getDetailPage = async (slug: CollectionSlug, id: string): Promise<PopularPackages> => {
   const result = await payload.findByID({
     collection: slug, // required
     id, // required
     depth: 1,
   })
-  return result
+  return result as PopularPackages
 }
 
 export const getPackages = async () => {
--- src/types/global.ts
+++ src/types/global.ts
@@ -4,4 +4,45 @@ export type PopularPackages = {
   cost: number
   destination: string
   package: string
+  overview?: string | null
+  highlights?: {
+    chips?:
+      | {
+          chip?: string
+          id?: string | null
+        }[]
+      | null
+    pointers?:
+      | {
+          pointer?: string | null
+          id?: string | null
+        }[]
+      | null
+  }
+  day_breakup?: {
+    days?:
+      | {
+          title?: string | null
+          break_up?:
+            | {
+                title?: string | null
+                subtitle?: string | null
+                id?: string | null
+              }[]
+            | null
+          id?: string | null
+        }[]
+      | null
+  }
+  accomodations?: {
+    location?:
+      | {
+          name?: string | null
+          type?: string | null
+          whats_included?: string | null
+          link?: string | null
+          id?: string | null
+        }[]
+      | null
+  }
 }
