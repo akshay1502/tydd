5d336d1096477d6cf70c4bfdfdac2eca579cd611
akshay <akshay.shinde@ly.design>
2025-03-01 18:02:57 +0530

    fixed packages integration with cms

---
+++ src/collections/FixedPackages.ts
@@ -0,0 +1,137 @@
+import type { CollectionConfig } from 'payload'
+
+export const FixedPackages: CollectionConfig = {
+  slug: 'fixed-packages',
+  access: {
+    read: () => true, // ✅ Make "GET /api/packages" public
+    create: () => true, // ✅ Allow public to add data (optional)
+    update: () => true, // ❌ Prevent public updates (optional)
+    delete: () => true, // ❌ Prevent public deletes (optional)
+  },
+  fields: [
+    {
+      name: 'destination',
+      type: 'text',
+    },
+    {
+      name: 'package',
+      type: 'text',
+    },
+    {
+      name: 'start_date',
+      type: 'date',
+    },
+    {
+      name: 'end_date',
+      type: 'date',
+    },
+    {
+      name: 'cost',
+      type: 'number',
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'category',
+      type: 'select',
+      options: ['Solo', 'Adventure', 'Honeymoon'],
+    },
+    {
+      name: 'gallery',
+      type: 'upload',
+      relationTo: 'media',
+      hasMany: true,
+    },
+    {
+      name: 'overview',
+      type: 'textarea',
+    },
+    {
+      name: 'highlights',
+      type: 'group',
+      fields: [
+        {
+          name: 'chips',
+          type: 'array',
+          fields: [
+            {
+              name: 'chip',
+              type: 'text',
+            },
+          ],
+        },
+        {
+          name: 'pointers',
+          type: 'array',
+          fields: [
+            {
+              name: 'pointer',
+              type: 'text',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'day_breakup',
+      type: 'group',
+      fields: [
+        {
+          name: 'days',
+          type: 'array',
+          fields: [
+            {
+              name: 'title',
+              type: 'text',
+            },
+            {
+              name: 'break_up',
+              type: 'array',
+              fields: [
+                {
+                  name: 'title',
+                  type: 'text',
+                },
+                {
+                  name: 'subtitle',
+                  type: 'text',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'accomodations',
+      type: 'group',
+      fields: [
+        {
+          name: 'location',
+          type: 'array',
+          fields: [
+            {
+              name: 'name',
+              type: 'text',
+            },
+            {
+              name: 'type',
+              type: 'text',
+            },
+            {
+              name: 'whats_included',
+              type: 'text',
+            },
+            {
+              name: 'link',
+              type: 'text',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
--- src/components/cards/fixedDeparture.tsx
+++ src/components/cards/fixedDeparture.tsx
@@ -1,4 +1,5 @@
 import IconCalendar from '@/assets/icons/calendar'
+import { FixedPackages } from '@/types/global'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -6,24 +7,43 @@ const Pill = ({ text }: { text: string }) => {
   return <button className="text-sm py-1 px-3 rounded-full bg-blue text-white">{text}</button>
 }
 
-export default function FixedDeparture() {
+export default function FixedDeparture({ data }: { data: FixedPackages }) {
   return (
-    <Link href="/" className="flex flex-col w-[300px] border border-offWhite rounded-lg">
+    <Link
+      href={`/package/fixed-packages/${data?.id}`}
+      className="flex flex-col w-[300px] border border-offWhite rounded-lg"
+    >
       <div className="relative rounded-lg overflow-hidden shadow-packages">
         <div className="w-[300] h-56">
-          <Image src="/destination.jpg" alt="image" fill className="object-cover" />
+          <Image src={data?.image?.url} alt={data?.image?.alt} fill className="object-cover" />
         </div>
-        <p className="absolute bottom-3 left-3 text-white font-semibold text-xl">Maldives</p>
+        <p className="absolute bottom-3 left-3 text-white font-semibold text-xl">
+          {data?.destination}
+        </p>
       </div>
       <div className="flex flex-col gap-2 p-2">
         <div className="flexCenter">
-          <p className="text-black">3 days & 4 Nights</p>
-          <Pill text="Group" />
+          <p className="text-black">{data?.package}</p>
+          {/* <Pill text="Group" /> */}
         </div>
-        <p className="text-black font-semibold">₹1,20,000 per person</p>
+        <p className="text-black font-semibold">
+          ₹{data?.cost?.toLocaleString('en-IN')} per person
+        </p>
         <div className="text-black font-semibold flex items-center gap-1.5">
           <IconCalendar />
-          <p className="text-black">Sat,11 Jan - Wed,15 Jan</p>
+          <p className="text-black">
+            {new Date(data?.start_date as string).toLocaleDateString('en-GB', {
+              weekday: 'short',
+              day: 'numeric',
+              month: 'short',
+            })}{' '}
+            -{' '}
+            {new Date(data?.end_date as string).toLocaleDateString('en-GB', {
+              weekday: 'short',
+              day: 'numeric',
+              month: 'short',
+            })}
+          </p>
         </div>
       </div>
     </Link>
