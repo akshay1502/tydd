a19d6ef906365e8721177ea1abf39e0be5b31e24
akshay <akshay.shinde@ly.design>
2025-03-09 16:25:17 +0530

    added depth and importing circular slider

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -14,19 +14,19 @@ import Marquee from 'react-fast-marquee'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import { Package } from '@/payload-types'
 import CircularSlider from '@/components/ImageCircularSlider'
 
 export default async function HomePage() {
-  const packages = await getPackages()
+  const packages = await getPackages(10)
   const fixedPackages = await getFixedPackages()
   const lastMinutePackages = await getLastMinutePackages()
-  const homeData = await getHomeData()
+  const homeData = await getHomeData(2)
 
   return (
     <div className="flex flex-col gap-28 pt-10 pb-20">
-      {/* <CircularSlider /> */}
       {/* Hero packages will redirect to same as popular packages */}
-      <SwiperHeroPackages data={homeData?.hero_Packages} />
+      <SwiperHeroPackages data={(homeData?.hero_Packages as Package[]) || []} />
       {/* packages with popular boolean marked as true */}
       <SwiperPopularPackages title="Popular Packages" data={packages} />
       {/* last minute date packages with discount */}
@@ -38,7 +38,10 @@ export default async function HomePage() {
       {/* fixed date packages */}
       <SwiperFixedPackages title="Fixed Departures" data={fixedPackages} />
       {/* packages with international type */}
-      <SwiperInternationalPackages title="International Budget-Friendly Packages" data={packages} />
+      <SwiperInternationalPackages
+        title="International Budget-Friendly Packages"
+        data={packages?.filter((item) => item?.type == 'international')}
+      />
       <div className="px-20">
         <h2 className="text-darkBlue text-[40px] leading-[48px] font-bold">Why TYDD</h2>
         <div className="mt-14 flex gap-32">
@@ -111,6 +114,7 @@ export default async function HomePage() {
           />
         </Marquee>
       </div>
+      <CircularSlider data={homeData?.testimonials_home ?? []} />
     </div>
   )
 }
