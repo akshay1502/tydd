e5d66e0187393d7dba4a09b859ae16c0c54b3955
akshay <akshay.shinde@ly.design>
2025-04-06 00:59:21 +0530

    used webp image for last minute background

--- src/components/Swiper/lastMinutePackages.tsx
+++ src/components/Swiper/lastMinutePackages.tsx
@@ -23,7 +23,7 @@ export default function SwiperLastMinutePackages({
   return (
     <div className="lg:mx-20">
       <div className="py-16 lg:py-20 relative lg:rounded-3xl overflow-hidden">
-        <Image src="/last_minute_bg.jpg" alt="image" fill className="object-cover -z-10" />
+        <Image src="/last_minute_bg.webp" alt="image" fill className="object-cover -z-10" />
         <div className="absolute w-full h-full top-0 bottom-0 left-0 right-0 bg-[#000] opacity-60 -z-10"></div>
         <div className="lg:px-16 px-4 flexCenter">
           <h2 className="text-white ">{title}</h2>
@@ -70,7 +70,7 @@ export default function SwiperLastMinutePackages({
               spaceBetween: 24, // 20px space between slides
             },
           }}
-          className="mySwiper !lg:px-16 !px-4"
+          className="mySwiper lg:!px-16 !px-4"
         >
           {data.map((data) => (
             <SwiperSlide key={data?.id} className="!w-auto">
