78ea31eb8e1a1c974670fdbfbf8b73064ce052f7
akshay <akshay.shinde@ly.design>
2025-04-13 01:41:29 +0530

    css fixes

--- src/assets/icons/swiperRight.tsx
+++ src/assets/icons/swiperRight.tsx
@@ -1,6 +1,13 @@
 export default function IconRight() {
   return (
-    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="lg:h-[28px] lg:w-[28px] h-[20px] w-[20px]"
+    >
       <path
         d="M16.8346 6.91833L23.9163 14L16.8346 21.0817"
         stroke="#2986FD"
