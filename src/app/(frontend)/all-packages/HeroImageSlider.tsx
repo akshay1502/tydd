f94e3708844fd2384854ff16ac69b9efe62b6985
akshay <akshay.shinde@ly.design>
2025-04-13 23:36:25 +0530

    all packages types images updated

--- src/app/(frontend)/all-packages/HeroImageSlider.tsx
+++ src/app/(frontend)/all-packages/HeroImageSlider.tsx
@@ -6,6 +6,47 @@ import { Suspense } from 'react'
 import PackagesTab from './PackagesTab'
 import { useSearchParams } from 'next/navigation'
 
+const packagesImages = [
+  {
+    type: 'domestic',
+    images: [
+      '/domestic1_compressed.webp',
+      '/domestic2_compressed.webp',
+      '/domestic3_compressed.webp',
+      '/domestic4_compressed.webp',
+      '/domestic5_compressed.webp',
+      '/domestic6_compressed.webp',
+      '/domestic7_compressed.webp',
+      '/domestic8_compressed.webp',
+    ],
+  },
+  {
+    type: 'international',
+    images: [
+      '/international1_compressed.webp',
+      '/international2_compressed.webp',
+      '/international3_compressed.webp',
+      '/international4_compressed.webp',
+      '/international5_compressed.webp',
+      '/international6_compressed.webp',
+      '/international7_compressed.webp',
+      '/international8_compressed.webp',
+    ],
+  },
+  {
+    type: 'cruise',
+    images: [
+      '/cruise1_compressed.webp',
+      '/cruise2_compressed.webp',
+      '/cruise3_compressed.webp',
+      '/cruise4_compressed.webp',
+      '/cruise5_compressed.webp',
+      '/cruise6_compressed.webp',
+      '/cruise7_compressed.webp',
+      '/cruise8_compressed.webp',
+    ],
+  },
+]
 export default function HeroImageSlider() {
   const searchParams = useSearchParams()
   const activeTab = searchParams.get('type') || 'domestic'
@@ -59,49 +100,51 @@ export default function HeroImageSlider() {
     )
   }, [activeTab])
 
+  const activeImages = packagesImages.find((item) => item.type === activeTab)?.images || []
+
   return (
     <div className="lg:h-[516px] h-[260px] flex gap-4 relative lg:mx-20 lg:rounded-3xl overflow-hidden bg-[#373535]">
       <div className="flex-[1.5] h-full grid grid-cols-2 grid-rows-[7fr_10fr] gap-4 relative">
         {/* Image 1 */}
         <div className="relative col-span-2 row-span-1 rounded-br-lg overflow-hidden bg-[#1E1F23]">
-          <Image src="/image1.webp" alt="Image 1" fill className="firstImage object-cover" />
+          <Image src={activeImages[0]} alt="Image 1" fill className="firstImage object-cover" />
         </div>
 
         {/* Image 2 */}
         <div className="relative col-span-1 row-span-1 rounded-tr-lg overflow-hidden bg-[#1E1F23]">
-          <Image src="/image2.webp" alt="Image 2" fill className="secondImage object-cover" />
+          <Image src={activeImages[1]} alt="Image 2" fill className="secondImage object-cover" />
         </div>
 
         {/* Image 3 */}
         <div className="relative col-span-1 row-span-1 rounded-t-lg overflow-hidden bg-[#1E1F23]">
-          <Image src="/image3.webp" alt="Image 3" fill className="thirdImage object-cover" />
+          <Image src={activeImages[2]} alt="Image 3" fill className="thirdImage object-cover" />
         </div>
       </div>
       <div className="lg:flex-[1.5] flex-[1] h-full grid grid-cols-1 lg:grid-cols-2 grid-rows-[10fr_7fr] gap-4 relative">
         {/* Image 1 */}
         <div className="relative col-span-2 lg:col-span-1 row-span-1 rounded-b-lg overflow-hidden bg-[#1E1F23]">
-          <Image src="/image3.webp" alt="Image 1" fill className="fourthImage object-cover" />
+          <Image src={activeImages[3]} alt="Image 1" fill className="fourthImage object-cover" />
         </div>
 
         {/* Image 2 */}
         <div className="hidden lg:block relative col-span-1 row-span-1 rounded-b-lg overflow-hidden bg-[#1E1F23]">
-          <Image src="/image2.webp" alt="Image 2" fill className="fifthImage object-cover" />
+          <Image src={activeImages[4]} alt="Image 2" fill className="fifthImage object-cover" />
         </div>
 
         {/* Image 3 */}
         <div className="relative col-span-2 row-span-1 rounded-t-lg overflow-hidden bg-[#1E1F23]">
-          <Image src="/image1.webp" alt="Image 3" fill className="sixthImage object-cover" />
+          <Image src={activeImages[5]} alt="Image 3" fill className="sixthImage object-cover" />
         </div>
       </div>
       <div className="hidden flex-1 h-full lg:grid grid-cols-1 grid-rows-2 gap-4 relative">
         {/* Image 1 */}
         <div className="relative col-span-1 row-span-1 rounded-bl-lg overflow-hidden bg-[#1E1F23]">
-          <Image src="/image2.webp" alt="Image 1" fill className="seventhImage object-cover" />
+          <Image src={activeImages[6]} alt="Image 1" fill className="seventhImage object-cover" />
         </div>
 
         {/* Image 2 */}
         <div className="relative col-span-1 row-span-1 rounded-tl-lg overflow-hidden bg-[#1E1F23]">
-          <Image src="/image3.webp" alt="Image 2" fill className="eightImage object-cover" />
+          <Image src={activeImages[7]} alt="Image 2" fill className="eightImage object-cover" />
         </div>
       </div>
       <div className="bg-all-packages-images-gradient z-10 absolute w-full h-full top-0 bottom-0 left-0 right-0"></div>
