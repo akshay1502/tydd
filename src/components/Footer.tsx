1e842a1d3fd5c82577e8c7734c7e0f8f7e7d0614
akshay <akshay.shinde@ly.design>
2025-04-13 21:11:40 +0530

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,34 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Footer() {
+  const footerData = [
+    {
+      header: 'Destination',
+      links: [
+        { name: 'Maldives', href: '/package/packages/Maldives' },
+        { name: 'Mauritius', href: '/package/packages/Mauritius' },
+        { name: 'Sri Lanka', href: '/package/packages/Sri-Lanka' },
+      ],
+    },
+    {
+      header: 'General',
+      links: [
+        { name: 'Domestic packages', href: '/all-packages?type=domestic' },
+        { name: 'International packages', href: '/all-packages?type=international' },
+        { name: 'Cruise packages', href: '/all-packages?type=cruise' },
+        { name: 'About us', href: '/about' },
+        { name: 'Contact us', href: '/contact-us' },
+      ],
+    },
+    {
+      header: 'Reach us',
+      links: [
+        { name: 'info@tydd.in', href: 'mailto:info@tydd.in' },
+        { name: 'Connect over Whatsapp', href: 'https://wa.me/917891234560' },
+        { name: '+91-7891234560', href: 'tel:+917891234560' },
+      ],
+    },
+  ]
   return (
     <footer className="relative px-4 pt-10 lg:px-20 lg:pt-12">
       <Image src="/footerBackground.webp" alt="footer" fill className="object-cover -z-10" />
@@ -20,88 +48,20 @@ export default function Footer() {
             <IconLinkedIn />
           </div>
         </div>
-        <div>
-          <h4 className="text-white">Destination</h4>
-          <ul className="mt-4">
-            <li className="lg:py-2">
-              <Link
-                href="/package/packages/Maldives"
-                className="text-lg font-semibold lg:font-normal lg:text-xl text-offWhite"
-              >
-                Maldives
-              </Link>
-            </li>
-            <li className="pt-2 lg:py-2">
-              <Link
-                href="/package/packages/Mauritius"
-                className="text-lg font-semibold lg:font-normal lg:text-xl text-offWhite"
-              >
-                Mauritius
-              </Link>
-            </li>
-            <li className="pt-2 lg:py-2">
-              <Link
-                href="/package/packages/Sri-Lanka"
-                className="text-lg font-semibold lg:font-normal lg:text-xl text-offWhite"
-              >
-                Sri Lanka
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h4 className="text-white">General</h4>
-          <ul className="mt-4">
-            <li className="lg:py-2">
-              <Link
-                href="/all-packages?type=domestic"
-                className="text-lg font-semibold lg:font-normal lg:text-xl text-offWhite"
-              >
-                Domestic packages
-              </Link>
-            </li>
-            <li className="pt-2 lg:py-2">
-              <Link
-                href="/all-packages?type=international"
-                className="text-lg font-semibold lg:font-normal lg:text-xl text-offWhite"
-              >
-                International packages
-              </Link>
-            </li>
-            <li className="pt-2 lg:py-2">
-              <Link
-                href="/all-packages?type=cruise"
-                className="text-lg font-semibold lg:font-normal lg:text-xl text-offWhite"
-              >
-                Cruise packages
-              </Link>
-            </li>
-            <li className="pt-2 lg:py-2">
-              <Link
-                href="/about"
-                className="text-lg font-semibold lg:font-normal lg:text-xl text-offWhite"
-              >
-                About us
-              </Link>
-            </li>
-            <li className="pt-2 lg:py-2">
-              <Link
-                href="/contact-us"
-                className="text-lg font-semibold lg:font-normal lg:text-xl text-offWhite"
-              >
-                Contact us
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h4 className="text-white">Reach us</h4>
-          <ul className="mt-4">
-            <li className="lg:py-2 text-offWhite">Info@tydd.com</li>
-            <li className="pt-2 lg:py-2 text-offWhite">Connect over Whatsapp</li>
-            <li className="pt-2 lg:py-2 text-offWhite">+91-7891234560</li>
-          </ul>
-        </div>
+        {footerData.map((item, index) => (
+          <div key={index} className="lg:py-2">
+            <h4 className="text-white h4 font-bold">{item.header}</h4>
+            <ul className="mt-4">
+              {item.links.map((link, index) => (
+                <li key={index} className="lg:py-2 py-0.5">
+                  <Link href={link.href} className="b2reg text-offWhite">
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
       <p className="text-xs lg:text-xl py-2 lg:py-4 text-center border-t border-offWhite mt-12 lg:mt-14 text-white">
         @2023 Travel Your Dream Destination. All rights reserved.
