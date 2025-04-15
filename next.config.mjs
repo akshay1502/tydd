4035629d8804068f6bdd9739fe702193706a4b01
akshay <akshay.shinde@ly.design>
2025-04-15 23:54:23 +0530

    removed bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-import withBundleAnalyzer from '@next/bundle-analyzer'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
 }
 
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-const combinedConfig = withPayload(bundleAnalyzer(nextConfig))
-
-export default combinedConfig
+export default withPayload(nextConfig)
