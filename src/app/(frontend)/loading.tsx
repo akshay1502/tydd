48fcc9dd378e8aac46feaa87acf95a1f93951d6a
akshay <akshay.shinde@ly.design>
2025-04-15 23:46:55 +0530

    added loader file

--- src/app/(frontend)/loading.tsx
+++ src/app/(frontend)/loading.tsx
@@ -1,23 +1,12 @@
+'use client'
+import animationData from '@/assets/lottie/loader2.json'
+import Lottie from 'lottie-react'
+
 export default function Loading() {
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen bg-white flex items-center justify-center z-[110]">
-      <p>loading</p>
+      <Lottie animationData={animationData} loop={true} className="w-[200px] h-[200px]" autoplay />
+      {/* <p>loading</p> */}
     </div>
   )
 }
-
-// 'use client'
-// // import Lottie from 'lottie-react'
-// import animationData from '@/assets/lottie/loader2.json'
-// import dynamic from 'next/dynamic'
-
-// const Lottie = dynamic(() => import('lottie-react'), { ssr: false })
-
-// export default function Loading() {
-//   return (
-//     <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen bg-white flex items-center justify-center">
-//       <Lottie animationData={animationData} loop={true} className="w-[200px] h-[200px]" autoplay />
-//       <p>loading</p>
-//     </div>
-//   )
-// }
