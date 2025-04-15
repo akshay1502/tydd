1bbc5999c076b0f340dc4d0d770c26efdc6e875b
akshay <akshay.shinde@ly.design>
2025-04-16 00:41:36 +0530

    loading as server side file

--- src/app/(frontend)/loading.tsx
+++ src/app/(frontend)/loading.tsx
@@ -1,12 +1,12 @@
-'use client'
-import animationData from '@/assets/lottie/loader2.json'
-import Lottie from 'lottie-react'
+// 'use client'
+// import animationData from '@/assets/lottie/loader2.json'
+// import Lottie from 'lottie-react'
 
 export default function Loading() {
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen bg-white flex items-center justify-center z-[110]">
-      <Lottie animationData={animationData} loop={true} className="w-[200px] h-[200px]" autoplay />
-      {/* <p>loading</p> */}
+      {/* <Lottie animationData={animationData} loop={true} className="w-[200px] h-[200px]" autoplay /> */}
+      <p>loading</p>
     </div>
   )
 }
