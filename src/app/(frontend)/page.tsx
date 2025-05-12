07d311093603ce7afd8a7ffa01d39444ffc0b08d
akshay <akshay.shinde@ly.design>
2025-05-13 00:11:41 +0530

    updated importmap

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,9 +2,11 @@ import Link from 'next/link'
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome To TYDD cms</h1>
-      <Link href="/admin">Go to Admin</Link>
+    <div className="lg:p-8 p-4">
+      <h1 className="font-bold text-3xl mb-8">Welcome To TYDD cms</h1>
+      <Link href="/admin" className="bg-black text-white py-2 px-4 rounded-sm">
+        Go to Admin
+      </Link>
     </div>
   )
 }
