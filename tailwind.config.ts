bf149c450329271cc8132cda572ad63b85b4da0e
akshay <akshay.shinde@ly.design>
2025-05-13 23:02:41 +0530

    tailwind import fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss'
-import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config: Config = {
   darkMode: ['class'],
@@ -110,7 +109,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [tailwindcssAnimate],
 } satisfies Config
 
 export default config
